35f1e66aca38239642f0d3988df4ebc43a0e4bc4
Khalid Hoffman <kahdev15@gmail.com>
2015-05-09 22:11:59 -0400

    - implemented basic server functions

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var mongoose = require('mongoose'),
     http = require("http");
 
+//var fs = require('fs'),
+//    index = fs.readFileSync('index.html');
+
 var username = 'kahdev15';
 var password = 'Clermont16';
 
 mongoose.connect('mongodb://'+username+':'+password+'@ds053370.mongolab.com:53370/com_khalidhoffman_mongodb');
 
 var db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'connection error:'));
+
 db.once('open', function (callback) {
     console.log('successfully connected.');
 });
 
 var TaskSchema = new mongoose.Schema({
+    id : String,
     name: String,
     dueDate: {type: Date, default: Date.now},
     notes: String,
@@ -27,28 +33,87 @@ var TaskSchema = new mongoose.Schema({
 
 var Task = mongoose.model('Task', TaskSchema, 'Deadlines');
 
-var newTask = new Task({
-    name: 'updated task name',
-    dueDate: new Date(),
-    notes: 'task Notes',
-    comments: []
-});
-
-//Task.findOneAndUpdate
-
-//newTask.save(function (err) {
-//    if (err) {
-//        console.log('error:', err);
-//    }
-//    console.log('success');
-//});
-
-
 http.createServer(function(request,response){
-    console.log(request);
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('hello world\n');
-    response.write('request:\n');
-    response.write(request.url);
-    response.end();
+    var urlArray  = request.url.split('/'),
+        op = urlArray[(urlArray.length-1)];
+
+    //console.log('request: ',request);
+    console.log('method:'+request.method);
+    //console.log('url:'+ URLParser.parse(request.url));
+    //console.log('headers: ', request.headers);
+    //request.on('close',function(){
+    //    console.log('close.trailers:', request.trailers);
+    //});
+
+    request.on('data', function(data) {
+        //if (data.name){
+        //    console.log(op, data);
+        //} else{
+        switch(request.method){
+            case 'DELETE':
+                var deletedTask = JSON.parse(data);
+                Task.remove({
+                    id : deletedTask.id
+                }, function (err) {
+                    if (err){
+                        console.log('Failed to remove task:', err);
+                    } else{
+                        console.log('Successfully removed task');
+                    }
+                });
+                break;
+            case 'PUT':
+            case 'POST':
+                var taskData = JSON.parse(data);
+                var newTask = new Task(taskData);
+                Task.findOneAndUpdate(
+                    { id: taskData.id},
+                    {
+                        name: taskData.name,
+                        dueDate: taskData.dueDate,
+                        notes: taskData.notes,
+                        comments: taskData.comments
+                    },
+                    {
+                        upsert: true
+                    }, function (err) {
+                        if (err){
+                            console.log('Failed to update task:', err);
+                        } else{
+                            console.log('Successfully updated task');
+                        }
+                    });
+                break;
+            default:
+                console.log('invalid method');
+                break;
+        }
+    });
+
+    response.setHeader("Content-Type", "text/html");
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    switch(request.method){
+        case 'GET':
+            // send collection json
+            response.setHeader('Content-Type', 'application/json');
+            Task.find({}, function (err, docs) {
+                if (err){
+                    console.log(err);
+                    return;
+                }
+                response.end(JSON.stringify(docs));
+            });
+            break;
+        default:
+            //console.log(op[(op.length-1)]);
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.write('Operation: invalid\n');
+            //console.log('Operation: invalid: '+op);
+            //console.log('trailers:', request.trailers);
+            response.end();
+            break;
+    }
 }).listen(process.env.PORT || 5000);
